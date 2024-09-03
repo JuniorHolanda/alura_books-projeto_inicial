be2062be1f0e2fc292120083d7351b71b290edcd
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-09-03 19:32:27 -0300

    update

---
+++ app/main.js
@@ -0,0 +1,10 @@
+let livros = [];
+const endpointApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+
+getBuscarLivrosDaApi()
+
+async function getBuscarLivrosDaApi() {
+    const res = await fetch(endpointApi)
+    livros = await res.json()
+    console.table(livros)
+}
